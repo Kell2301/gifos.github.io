30ba4fc8ad52d7814ea2cf8d5f103c1375b5c09c
Andres Rojas <carlos.rojas4538gmail.com>
2021-05-27 00:26:10 -0500

    Se corrigen errores cuando se consulta al appi

--- script/code.js
+++ script/code.js
@@ -5,24 +5,53 @@ const   api_key="api_key=GMg5tbTmfRwlCGLgyWd0DSZ8yTC4fvrG",
         api_search_tag="tag=",
         api_limit="limit=3",
         api_rating="rating=g",
-        gifs_container=document.querySelector('.trending-slider-contenedor .trending-slider-box')
-        
-const get_gifs= ()=>{
+        gifs_container=document.querySelector('.trending-slider-contenedor .trending-slider-box'),
+        listen_izq=document.querySelector('#trending-btn-previous'),
+        listen_der=document.querySelector('#trending-btn-next')
+        console.log(gifs_container)
 
-    fetch(`${api_url_base}${api_trending_endpointt}?${api_key}&${api_limit}&${api_rating}`)
+        // Se crean funciones para la lecturas de eventos en los botones IZQ o DER.
+const listen_id= (id_name,funcion_acive)=>{
+    const id_element= document.querySelector(`#${id_name}`)
+    id_element.addEventListener('click',funcion_acive)
+}
+
+// para hacer el paso de los gif en la home page podemos hacer el div externo con un overflow : hiden y luego ir cambiando la posicion de los elemento entro de ese vida asi podemos ponerle animacion y tambien controlar lo que se ve en la pantalla principal
+
+const listen_class= (class_name,funcion_acive)=>{
+    const class_elements= document.querySelectorAll(`.${class_name}`)
+    for (const class_element of class_elements) {
+        class_element.addEventListener('click',funcion_acive)
+    }
+}
+const _fech_app_process=(url, funtion_action)=>{
+    fetch(url)
     .then(res=>res.json())
     .then(res=>{
-        const gifs=res.data
-        for (const gif of gifs) {
-             get_gifs_url(gif.bitly_gif_url)
-        }
-       
+        for (let index = 0; index < res.data.length; index++) {
+            // console.log(res.data[index].bitly_url)
+            funtion_action(res.data[index].url)
+        }  
     }).catch(error=>console.error(error))
 }
+const show_previous_gif=()=>{
+    console.log("show gif")
+}
+get_gifs_url= url =>{
+    const content=`
+    <div class="trending-slider" id="trending-slider">
+        <img src="${url}" alt="img_gifs"></img>
+    </div>
+    `
+    gifs_container.insertAdjacentHTML("afterbegin",content)
+}
+
+const get_gifs= ()=>{
+    url=`${api_url_base}${api_trending_endpointt}?${api_key}&${api_limit}&${api_rating}`
+    _fech_app_process(url,get_gifs_url)
+}
+
 get_gifs()
 
-const listen=()=>{
-
-
-
-}
+listen_id("trending-btn-previous",show_previous_gif)
+listen_id("trending-btn-next",show_previous_gif)
